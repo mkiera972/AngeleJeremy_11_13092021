60fae707098b5b8c1f89eee0da2b7d7ec6c533a8
jérémy angele <jeremyangele@gmail.com>
2021-10-10 11:20:37 -0400

    Amélioration du visuel du titre de la bannière accueil en responsive
    De même pour le nom de l'auteur sur la page de l'annonce

--- kasaici/src/Home/Home.js
+++ kasaici/src/Home/Home.js
@@ -2,13 +2,16 @@ import React from 'react';
 import data from "./../data/data";
 import Adverts from "../Adverts/Adverts";
 
+/**
+ * PAGE ACCUEIL
+ */
 class Home extends React.Component {
     render() {
         return (
             <div className="container">
                 <div className="banner">
                     <img className="banner-img" alt="" src='/img/banner.png'/>
-                    <p className="banner-title">Chez vous, partout et ailleurs</p>
+                    <p className="banner-title">Chez vous, <span className="banner-title-sub">partout et ailleurs</span></p>
                 </div>
                 <div className="adverts">
                     <Adverts adverts={data} />
